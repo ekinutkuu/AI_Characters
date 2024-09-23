233bc3e62853d1982ac2e527310bfe7c808f1116
Ekin Utku <ekinutkugulmez@gmail.com>
2024-09-23 21:27:24 +0300

    basic design of the page and character cards

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
+import "./App.css";
+import CharacterCard from "./components/CharacterCard/CharacterCard";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+   return (
+      <div className="App">
+         <div className="characterCards">
+            <CharacterCard />
+            <CharacterCard />
+            <CharacterCard />
+            <CharacterCard />
+            <CharacterCard />
+            <CharacterCard />
+            <CharacterCard />
+         </div>
+      </div>
+   );
 }
 
 export default App;
---
+++ frontend/src/components/CharacterCard/CharacterCard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./CharacterCard.css";
+
+const CharacterCard = () => {
+   return (
+      <div className="characterCard">
+         <div className="character-name"></div>
+         <div className="character-description"></div>
+         <div className="character-image"></div>
+      </div>
+   );
+};
+
+export default CharacterCard;
